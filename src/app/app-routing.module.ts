5fc9458d5bbe90ff6d26c57611e4f483d311d98a
Emely Garcia <garciam.emm@gmail.com>
2020-07-07 21:34:28 -0600

    componentes de chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+import { MainChatComponent } from './components/main-chat/main-chat.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MainChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListContactsComponent } from './components/list-contacts/list-contacts.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MainChatComponent } from './components/main-chat/main-chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListContactsComponent,
+    ChatComponent,
+    MainChatComponent
   ],
   imports: [
     BrowserModule,
