17aa1213622b2aaecbe1323a94ace339fbf6a865
Emely Garcia <garciam.emm@gmail.com>
2020-07-15 15:04:15 -0600

    menu de opciones funcional

--- src/app/components/main-chat/main-chat.component.ts
+++ src/app/components/main-chat/main-chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { options } from './options';
 
 @Component({
   selector: 'app-main-chat',
@@ -8,21 +9,49 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class MainChatComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav;
-  
+  options:any[];
+  indexOption:number;
+  title_option:string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.indexOption -1;
+    this.options = options;
+    this.title_option='';
   }
 
   /**
    * Método para cerrar el sidenav
    */
   closeSideNav(){
+    this.indexOption = -1;
     this.sidenav.close();
   }
 
-  openSideNav(){
-    this.sidenav.open()
+  /**
+   * Método para abrir el sidenav
+   * @param index de opcion
+   */
+  openSideNav(index:number){
+    if(index!=this.indexOption){
+      this.title_option = this.options[index].title;
+      this.indexOption = index;
+      this.sidenav.open();
+    }else{
+      this.title_option = '';
+      this.indexOption = -1;
+      this.sidenav.close();
+    }
+
+  }
+
+  /**
+   * Método retornar el index en options segun el titulo
+   * @param tittle de la opcion
+   */
+  getIndex(tittle:string):number{
+    return this.options.findIndex(element=>element.title === tittle)
   }
 
 }
---
+++ src/app/components/main-chat/options.ts
@@ -0,0 +1,17 @@
+export const options = [
+    {
+        title: "Cuenta",
+        icon: "account_circle"
+    },
+    {
+        title: "Contactos",
+        icon: "contacts"
+    },
+    {
+        title: "Chats",
+        icon: "chat"
+    },
+    {
+        title: "Configuración",
+        icon: "settings"
+    }];
