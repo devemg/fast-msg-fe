f80aaddabb03ff56ef28f713af2ca2e5c703af59
Emely Garcia <garciam.emm@gmail.com>
2020-07-14 23:01:49 -0600

    maquetacion de chat con colores obscuros

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -8,164 +8,164 @@ import { ChatItemList } from '../models/chat-item-list.model';
 })
 export class ChatService {
 
-  private list_chat:ChatItemList[];
-  private list_contact:Contact[];
+  private list_chat: ChatItemList[];
+  private list_contact: Contact[];
 
-  constructor() { 
+  constructor() {
     this.list_chat = [
       {
-        contactImage:'image',
-        contactName:'chat 1',
-        id:1,
-        lastMessage:{
-          id:1,
-          content:"Holis",
+        contactImage: 'image',
+        contactName: 'chat 1',
+        id: 1,
+        lastMessage: {
+          id: 1,
+          content: "Holis",
           date: new Date(),
-          owner:1
+          owner: 1
         }
       }
     ];
 
     this.list_contact = [
       {
-        id:1,
-        image:'image',
-        name:'Nombreee 1'
+        id: 1,
+        image: 'image',
+        name: 'Nombreee 1'
       },
       {
-        id:2,
-        image:'image',
-        name:'Nombreee 2'
+        id: 2,
+        image: 'image',
+        name: 'Nombreee 2'
       },
       {
-        id:3,
-        image:'image',
-        name:'Nombreee 3'
+        id: 3,
+        image: 'image',
+        name: 'Nombreee 3'
       }
     ];
   }
 
-  async getChats():Promise<ChatItemList[]>{
+  async getChats(): Promise<ChatItemList[]> {
     return this.list_chat;
   }
 
-  async getContacts():Promise<Contact[]>{
+  async getContacts(): Promise<Contact[]> {
     return this.list_contact;
   }
 
-  async getChat(id:number):Promise<Chat>{
+  async getChat(id: number): Promise<Chat> {
     return {
-      id:1,
-      contactImage:"Contacto 1",
-      contactName:"image",
-      messages:[
+      id: 1,
+      contactImage: "Contacto 1",
+      contactName: "image",
+      messages: [
         {
-          id:1,
-          content:"holiiiiis",
-          date:new Date(),
-          owner:1
+          id: 1,
+          content: "holiiiiis",
+          date: new Date(),
+          owner: 1
         }
-        ,{
-          id:2,
-          content:"holiiiiis x2",
-          date:new Date(),
-          owner:2
+        , {
+          id: 2,
+          content: "holiiiiis x2",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:3,
-          content:"holiiiiis x3",
-          date:new Date(),
-          owner:1
+          id: 3,
+          content: "holiiiiis x3",
+          date: new Date(),
+          owner: 1
         },
         {
-          id:4,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 4,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:5,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 5,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:1,
-          content:"holiiiiis",
-          date:new Date(),
-          owner:1
+          id: 1,
+          content: "holiiiiis",
+          date: new Date(),
+          owner: 1
         }
-        ,{
-          id:2,
-          content:"holiiiiis x2",
-          date:new Date(),
-          owner:2
+        , {
+          id: 2,
+          content: "holiiiiis x2",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:3,
-          content:"holiiiiis x3",
-          date:new Date(),
-          owner:1
+          id: 3,
+          content: "holiiiiis x3",
+          date: new Date(),
+          owner: 1
         },
         {
-          id:4,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 4,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:1,
-          content:"holiiiiis",
-          date:new Date(),
-          owner:1
+          id: 1,
+          content: "holiiiiis",
+          date: new Date(),
+          owner: 1
         }
-        ,{
-          id:2,
-          content:"holiiiiis x2",
-          date:new Date(),
-          owner:2
+        , {
+          id: 2,
+          content: "holiiiiis x2",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:3,
-          content:"holiiiiis x3",
-          date:new Date(),
-          owner:1
+          id: 3,
+          content: "holiiiiis x3",
+          date: new Date(),
+          owner: 1
         },
         {
-          id:4,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 4,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:5,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 5,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:1,
-          content:"holiiiiis",
-          date:new Date(),
-          owner:1
+          id: 1,
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          date: new Date(),
+          owner: 1
         }
-        ,{
-          id:2,
-          content:"holiiiiis x2",
-          date:new Date(),
-          owner:2
+        , {
+          id: 2,
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          date: new Date(),
+          owner: 2
         },
         {
-          id:3,
-          content:"holiiiiis x3",
-          date:new Date(),
-          owner:1
+          id: 3,
+          content: "holiiiiis x3",
+          date: new Date(),
+          owner: 1
         },
         {
-          id:4,
-          content:"holiiiiis x4",
-          date:new Date(),
-          owner:2
+          id: 4,
+          content: "holiiiiis x4",
+          date: new Date(),
+          owner: 2
         }
       ]
     };
