7311f34d65e646a4e03ed6c27d5d2c02636d8f3e
Emely Garcia <garciam.emm@gmail.com>
2020-07-20 21:59:58 -0600

    envio y recepcion de mensajes

--- src/app/chat/components/chat/chat.component.ts
+++ src/app/chat/components/chat/chat.component.ts
@@ -1,47 +1,85 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Chat } from '../../models/chat.model';
 import { ChatService } from '../../services/chat.service';
 import { FormControl } from '@angular/forms';
 import { SocketChatService } from '../../services/socket-chat.service';
+import { SesionService } from '../../services/sesion.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit,OnChanges {
-  @Input() idChat:string;
+export class ChatComponent implements OnInit, OnChanges, AfterViewInit {
+  @Input() idChat: string;
+  //scroll
+ // @ViewChild('scrollMe') scroll : ElementRef;
+  //scrolltop:number=0;
 
-  chat:Chat;
-  idUser:string;
+  chat: Chat;
+  idUser: string;
 
-  mesg:FormControl;
+  mesg: FormControl;
 
   constructor(
-    private chatService:ChatService,
-    private socketService:SocketChatService
+    private chatService: ChatService,
+    private sesionService:SesionService,
+    private socketService: SocketChatService
   ) { }
 
   ngOnInit(): void {
     this.mesg = new FormControl('');
+    this.idUser = this.sesionService.getUserID();
+    this.subscribeAll();
   }
 
-  ngOnChanges():void{
-    if(this.idChat){
+  ngOnChanges(): void {
+    if (this.idChat) {
       this.getChat()
     }
   }
 
-  async getChat(){
+  ngAfterViewInit(){
+   // this.updateScroll();
+  }
+
+  /**
+   * consulta la info del chat
+   */
+  async getChat() {
     this.chat = await this.chatService.getChat(this.idChat);
   }
 
-  send(){
-    if(this.mesg.valid){
-      console.log(this.mesg.value)
-      this.socketService.sendMessage(this.mesg.value,this.chat._id)
+  /**
+   * envía el mensaje por sockets
+   */
+  send() {
+    if (this.mesg.valid) {
+      //console.log(this.mesg.value)
+      this.socketService.sendMessage(this.mesg.value, this.chat._id)
       this.mesg.reset()
     }
   }
 
+  /**
+   * suscripción a los eventos enviados por el socket
+   */
+  subscribeAll() {
+    this.socketService.onMessage()
+      .subscribe(data => {
+        if (data) {
+          if (data.chat == this.chat._id) {
+            this.chat.messages.push(data.message);
+           // this.updateScroll();
+          }
+        }
+      })
+  }
+
+ /* updateScroll(){
+    if(this.scroll){
+      this.scrolltop = this.scroll.nativeElement.scrollHeight;
+    }
+  }
+*/
 }
--- src/app/chat/services/socket-chat.service.ts
+++ src/app/chat/services/socket-chat.service.ts
@@ -2,29 +2,60 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 import { SesionService } from './sesion.service';
+import { Message } from '../models/message.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SocketChatService {
 
-  socket:any;
+  socket: any;
 
   constructor(
-    private sesionService:SesionService
+    private sesionService: SesionService
   ) { }
 
 
-  registerInSocketChat(){
+  /**
+   * Registra al usuario en la lista de conectados 
+   * para recibir mensajes
+   */
+  registerInSocketChat() {
     this.socket = io(environment.ENDPOINT_SOCKET);
-    if(this.socket){
-      this.socket.emit('register',this.sesionService.getUserID()); 
+    if (this.socket) {
+      this.socket.emit('register', this.sesionService.getUserID());
     }
   }
 
-  sendMessage(message,chatId){
-    if(this.socket){
-      this.socket.emit('chat-message',{owner:this.sesionService.getUserID(),chatId,message}); 
+  /**
+   * Envía un mensaje
+   * @param message contenido del mensaje
+   * @param chatId 
+   */
+  sendMessage(message, chatId) {
+    if (this.socket) {
+      this.emit('chat-message',{ owner: this.sesionService.getUserID(), chatId, message });
     }
+  }
+
+  /**
+  * Retorna un Observable para recibir los mensajes
+  */
+  public onMessage(): Observable<any> {
+    return this.listen('chat-message');
+  }
+
+
+  private emit(eventName:string,data:any){
+    if(this.socket){
+      this.socket.emit(eventName,data)
+    }
+  }
+
+  public listen(eventName:string){
+    return new Observable((subscriber=>{
+      this.socket.on(eventName,(data)=>subscriber.next(data));
+    }))
   }
 
 }
