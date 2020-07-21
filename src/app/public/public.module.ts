b748b8f30d1f226547cc02373e86301c582246d0
Emely Garcia <garciam.emm@gmail.com>
2020-07-21 11:47:36 -0600

    agregando componente de contacto y cards en page

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -8,10 +8,11 @@ import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { ConfirmAccountComponent } from './components/confirm-account/confirm-account.component';
 import { MaterialAngularModule } from '../shared-modules/material-angular/material-angular.module';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, PageComponent, RegisterComponent, ResetPasswordComponent, ConfirmAccountComponent],
+  declarations: [LoginComponent, PageComponent, RegisterComponent, ResetPasswordComponent, ConfirmAccountComponent, ContactComponent],
   imports: [
     CommonModule,
     PublicRoutingModule,
