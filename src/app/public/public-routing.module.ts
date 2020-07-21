046682d44d9caa0fa320ca0902e58f348dfd854d
Emely Garcia <garciam.emm@gmail.com>
2020-07-21 17:42:35 -0600

    redireccionamiento a paginas publicas

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageComponent } from './components/page/page.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ConfirmAccountComponent } from './components/confirm-account/confirm-account.component';
 
 
 const routes: Routes = [
@@ -12,6 +16,22 @@ const routes: Routes = [
   {
     path:'login',
     component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'reset-password',
+    component:ResetPasswordComponent
+  },
+  {
+    path:'confirm-account/:id',
+    component:ConfirmAccountComponent
   }
 ];
 
