a9a9ab72cfd6b4752a3bb3bd5a1bf52db90b8816
Emely Garcia <garciam.emm@gmail.com>
2020-07-22 19:32:02 -0600

    login user api

--- src/app/public/components/login/login.component.ts
+++ src/app/public/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { getFormValidationErrors, handleError } from 'src/assets/extra-functions';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   loginForm:FormGroup;
 
   constructor(
-    private formBulder:FormBuilder
+    private formBulder:FormBuilder,
+    private authService:AuthService
   ) { }
 
   ngOnInit(): void {
@@ -25,7 +27,13 @@ export class LoginComponent implements OnInit {
 
   submit(){
     if(this.loginForm.valid){
-      console.log(this.loginForm.value)
+      this.authService.login(this.loginForm.value)
+      .subscribe(data=>{
+        console.log(data)
+      },
+      error=>{
+        console.error("Error",error)
+      })
     }else{
       //getFormValidationErrors(this.loginForm)
     }
