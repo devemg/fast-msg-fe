c4fa1897a7597e2dfee68c23ab153b158ecc52ed
Emely Garcia <garciam.emm@gmail.com>
2020-08-03 14:46:11 -0600

    menu responsive

--- src/app/public/components/main-page/main-page.component.ts
+++ src/app/public/components/main-page/main-page.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss','./main-page-resp.component.scss']
 })
 export class MainPageComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
