997f5d6183b86622779d415dfb3317d8fd550c28
Emely Garcia <garciam.emm@gmail.com>
2020-08-03 17:11:30 -0600

    sidenav close en public

--- src/app/public/components/main-page/main-page.component.ts
+++ src/app/public/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss','./main-page-resp.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  closeSideNav(){
+    this.sidenav.close()
   }
 
 }
