29415b54f6584628242edd9a69321cf8ed1607b4
devemg <garciam.emm@gmail.com>
2021-06-15 08:34:36 -0600

    rename and relocate modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path:'',
-    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+    loadChildren: () => import('./modules/Public/public.module').then(m => m.PublicModule)
   },
   {
     path:'user',
-    loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)
+    loadChildren: () => import('./modules/users/chat.module').then(m => m.ChatModule)
   }
 ];
 
