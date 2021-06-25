634200fb372af561d163f5fb1c0f2b2e6b7ece6c
devemg <garciam.emm@gmail.com>
2021-06-25 17:49:00 -0600

    move guard

--- src/app/services/guards/auth.guard.ts
+++ src/app/services/guards/auth.guard.ts
@@ -9,11 +9,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.log('ROUTE',route)
-      console.log('STATE',state)
-    return new Promise((resolve,reject)=>{
-      resolve(true)
-    });
+    return true;
   }
   
 }
--- src/app/services/random-data.service.spec.ts
+++ src/app/services/random-data.service.spec.ts
@@ -10,7 +10,34 @@ describe('RandomDataService', () => {
     service = TestBed.inject(RandomDataService);
   });
 
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return list of random contacts', () => {
+    expect(service.getContacts(2).length).toBe(2);
+  });
+
+  it('should get user profile', () => {
+    expect(service.getUser()).toBeDefined();
+  });
+
+  it('should get list of chats ', () => {
+    expect(service.getChatList(4).length).toBe(4);
+  });
+
+  it('should get chat ', () => {
+    expect(service.getChat()).toBeDefined();
+  });
+
+  it('should get list of messages ', () => {
+    expect(service.getRandomMessages(4).length).toBe(4);
+  });
+
+  it('should get random number between 10 and 25', () => {
+    expect(service.getRandomNumber()).toBeGreaterThanOrEqual(10);
+    expect(service.getRandomNumber()).toBeLessThanOrEqual(25);
+  });
+  
 });
