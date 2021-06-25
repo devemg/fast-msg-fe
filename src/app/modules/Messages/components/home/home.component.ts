9502904cb9aed5680a8692ebf41a7e5aefcb9b18
devemg <garciam.emm@gmail.com>
2021-06-24 18:05:33 -0600

    hidde menu

--- src/app/modules/Messages/components/home/home.component.ts
+++ src/app/modules/Messages/components/home/home.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component, OnInit, ViewChild, OnChanges, AfterViewInit } from '@angular/core';
 import { MenuOption, options } from './options';
 import { Router } from '@angular/router';
-import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { AuthService } from 'src/app/services/auth.service';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
   options: MenuOption [];
  
-  constructor(private authService: AuthService, private router: Router) {
+  
+  constructor(private authService: AuthService, private router: Router, private breakpointObserver:BreakpointObserver) {
     this.options = options;
   }
 
-  ngOnInit() {
-    
+  ngOnInit() {  
+
   }
+
+  ngAfterViewInit() {
+    this.breakpointObserver.observe([
+      '(max-width: 550px)'
+        ]).subscribe(result => {
+          if (result.matches) {
+            this. sidenav.close();
+          }else {
+            this. sidenav.open();
+          }
+        }); 
+  }
+
 
   logout() {
     this.authService.logout();
