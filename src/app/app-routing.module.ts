c51aff217c1f0140e38c22219ad684774257557c
devemg <garciam.emm@gmail.com>
2021-06-25 18:10:36 -0600

    implementando guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './services/auth.guard';
+import { AuthGuard } from './services/guards/auth.guard';
 
 const routes: Routes = [
   {
     path:'',
     loadChildren: () => import('./modules/Public/public.module').then(m => m.PublicModule),
-    //canLoad:[ AuthGuard ]
   },
   {
     path:'user',
     loadChildren: () => import('./modules/messages/messages.module').then(m => m.MessagesModule),
-    //canLoad: [ AuthGuard ]
+    canLoad: [ AuthGuard ]
   }
 ];
 
--- src/app/services/guards/auth.guard.ts
+++ src/app/services/guards/auth.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+export class AuthGuard implements CanLoad {
+  
+  constructor(private authService: AuthService, private router: Router){}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(!this.authService.isAuth()){
+        this.router.navigate(['/login']);
+        return false;
+      }
     return true;
   }
-  
 }
